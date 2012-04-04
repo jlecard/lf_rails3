0235497249b30baf800570fff18991770293ce06
James Lecard <jameslecard@gmail.com>
2012-04-04 21:27:26 +0200

    generated specs for connectors

---
+++ spec/models/connectors/wcapi_spec.rb
@@ -0,0 +1,32 @@
+# generated file
+require 'spec_helper'
+require 'wcapi_search_class.rb'
+
+describe WcapiSearchClass do
+  describe "SearchCollection" do
+    without_transactional_fixtures do
+      it "should return records (no action_type)" do
+        klass = WcapiSearchClass.new
+        collection = Factory(:wcapi_collection)
+        records = klass.SearchCollection(collection, ["keyword"], ["johnson"], 0, 100, [], 10)
+        records.should be_a(Array)
+        records.size.should == 100
+        records[0].should be_a(Record)
+        records[99].should be_a(Record)
+      end
+      it "should return an id, hits and total_hits (action_type set)" do
+        klass = WcapiSearchClass.new
+        collection = Factory(:wcapi_collection)
+        id, hits, total_hits = klass.SearchCollection(collection, ["keyword"], ["jack"], 0, 100, [], 10, -1, nil, nil, nil, "test")
+        id.should match(/^\d+_\d+$/)
+        hits.should == 100
+        total_hits.should > 100
+        records = CACHE.get(id)
+        records.should be_a(InCacheRecord)
+        records.max.should == hits
+        records.total_hits.should == total_hits
+        records.status.should == 0
+      end
+    end
+  end
+end
